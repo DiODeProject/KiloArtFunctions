c555bc4d6d9c90a9f51947633ccf1f94f28cd3c8
Junjin Chen <jchen54@sheffield.ac.uk>
2018-06-14 15:24:44 +0100

    update: nested loop

--- BehaviourGenerator/generator.py
+++ BehaviourGenerator/generator.py
@@ -52,7 +52,10 @@ class BehaviourGenerator:
 		self.wfile = open(name, 'w')
 
 		result = ''
+		global_variable = ''
 		action_no = 0
+		nested_action = 0
+		nested_action_no = {}
 
 		content = self.rfile.readlines()
 		
@@ -60,27 +63,71 @@ class BehaviourGenerator:
 			parameters = re.split(r'\t+',line.rstrip('\n'))
 			try:
 				int(parameters[0])
-				result = result + 'if(current_action == ' + str(action_no) +  \
+
+				condition = 'if(current_action ==' if nested_action==0 else \
+					('if(inner_action' + str(nested_action) + ' ==')
+				condition_no = action_no if nested_action ==0 else \
+					nested_action_no[nested_action]
+				condition_action = 'current_action' if nested_action==0 else \
+					'inner_action'+str(nested_action)
+
+				result = result + condition + str(condition_no) +  \
 					' ) {\n' + \
 					'  set_motion('+parameters[2]+');\n' + \
 					'  set_colour('+parameters[1]+');\n' + \
 					'  if(kilo_ticks > last_state_update + ' + parameters[0] + ') {\n' + \
-					'    current_action += 1;\n' + \
+					'    '+condition_action+' += 1;\n' + \
 					'    last_state_update = kilo_ticks;\n' + \
 					'  }\n' + \
 					'}\n'
-				action_no += 1
+
+				if nested_action == 0:
+					action_no += 1
+				else:
+					nested_action_no[nested_action] += 1
 
 			except ValueError:
 				if parameters[0] == 'REPEAT':
-					result = result + 'for(int i=0;i<'+parameters[1]+';i++){\n' + \
-						'current_action = '+str(action_no)+';\n'
+
+					nested_action_no[nested_action+1] = 0
+					condition = 'if(current_action ==' if nested_action==0 else \
+						('if(inner_action' + str(nested_action) + ' ==')
+					condition_no = action_no if nested_action ==0 else \
+						nested_action_no[nested_action]
+
+					result = result + condition + str(condition_no) + \
+						' ) {\n\n' + \
+						'  int i'+str(nested_action+1)+' = 0;\n' + \
+						'  while(i'+str(nested_action+1)+'<'+parameters[1]+'){\n'
+					global_variable = global_variable + 'int inner_action'+str(nested_action+1)+' = 0;\n'
+					nested_action += 1
 				if parameters[0] == 'REPEAT END':
-					result = result + '}\n'
-		
-		print(result)
+
+					condition = '    if(inner_action' + str(nested_action) + ' =='
+					condition_no = nested_action_no[nested_action]
+					condition_action = 'current_action' if nested_action-1==0 else \
+						'inner_action'+str(nested_action-1)
+
+					result = result + condition + str(condition_no) + \
+						') {\n' + \
+						'      i'+str(nested_action)+'++;\n' + \
+						'      inner_action' + str(nested_action) + ' = 0;\n' + \
+						'    }\n' + \
+						'  }\n\n' + \
+						'  '+condition_action+' += 1;\n' + \
+						'}\n'
+
+					nested_action -= 1
+					if nested_action == 0:
+						action_no += 1
+					else:
+						nested_action_no[nested_action] += 1
 
 		result = self.temp_file.read().replace('//INSERT CODE HERE',result,1)
+		result = result.replace('//INSERT GLOBAL VARIABLES HERE',global_variable,1)
+
+		print(result)
+
 		self.wfile.write(result)
 
 if __name__ == '__main__':
